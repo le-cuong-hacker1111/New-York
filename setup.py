9c7a5d77d85a840759fc7ebbb033a4e793181a79
Heaven Hevin B <96363896+le-cuong-hacker1111@users.noreply.github.com>
2022-06-24 03:17:25 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,81 @@
 import os
-
 c = input("Choose your environment: [0] pip / [1] pip3 : ")
 
 if c == "0":
-
     os.system("pip install cloudscraper")
-
     os.system("pip install socks")
-
     os.system("pip install pysocks")
-
     os.system("pip install colorama")
-
     os.system("pip install httpx")
-
     os.system("pip install undetected_chromedriver")
-
+    os.system("pip install requests urllib3")
+    os.system("pip install pysocks5")
+    os.system("pip install requests")
+    os.system("pip install scapy")
+    os.system("pip install argparse")
+    os.system("pip install wget")
+    os.system("pip install humanfriendly")
 elif c == "1":
-
+ip install pysocks")
     os.system("pip3 install cloudscraper")
-
     os.system("pip3 install socks")
-
     os.system("pip3 install pysocks")
-
     os.system("pip3 install colorama")
-
-    os.system("pip3 install undetected_chromedriver")
-
     os.system("pip3 install httpx")
+    os.system("pip3 install undetected_chromedriver")
+    os.system("pip3 install requests urllib3")
+    os.system("pip3 install pysocks5")
+    os.system("pip3 install requests")
+    os.system("pip3 install scapy")
+    os.system("pip3 install argparse")
+    os.system("pip3 install wget")
+    os.system("pip install humanfriendly")
 
 if os.name == "nt":
-
     pass
-
 else:
-
     os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+    os.system("sudo apt-get install ./google-chrome-stable_current_amd64.deb")
+    os.system("sudo apt-get install golang")
+    os.system("sudo apt-get install python3")
+    os.system("sudo apt-get install python2")
+    os.system("sudo apt-get install python3-pip")
+    os.system("sudo apt-get install nodejs")
+    os.system("sudo apt-get install perl")
+    os.system("sudo apt-get install npm")
+    
+print("Done.")    
 
-    os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
 
-print("Done.")
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
